50166a84a7f1f62615bf64430b63fba47fd0d27b
kodebae <heykarmendurbin@gmail.com>
2025-09-12 17:57:36 -0400

    feat: updating pages and links

--- test/integration/static_pages_test.rb
+++ test/integration/static_pages_test.rb
@@ -4,7 +4,7 @@ class StaticPagesTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "h1", "Sample App"
+    assert_select "h1", "Welcome to Kodebae's Sample App"
   end
 
   test "should get help" do
@@ -23,5 +23,19 @@ class StaticPagesTest < ActionDispatch::IntegrationTest
     get contact_path
     assert_response :success
     assert_select "h1", "Contact"
+  end
+
+  test "should get root" do
+    get root_path
+    assert_response :success
+  end
+
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
   end
 end
