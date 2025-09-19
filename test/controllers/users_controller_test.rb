748ab467909f8ba2635c22cf4ce8d9ba15bfbd04
kodebae <heykarmendurbin@gmail.com>
2025-09-19 16:10:27 -0400

    feat: complete signup form functionality

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -15,9 +15,36 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get signup" do
+    get signup_path
+    assert_response :success
+  end
+
+  test "should reject invalid signup information" do
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                       email: "user@invalid",
+                                       password: "foo",
+                                       password_confirmation: "bar" } }
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "should accept valid signup information" do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                       email: "user@example.com",
+                                       password: "password",
+                                       password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+
   test "should create user" do
     assert_difference("User.count") do
-      post users_url, params: { user: { email: @user.email, name: @user.name } }
+      post users_url, params: { user: { email: @user.email, name: @user.name, 
+                                      password: "foobar", password_confirmation: "foobar" } }
     end
 
     assert_redirected_to user_url(User.last)
