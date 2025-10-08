0f4f8d3dd0ac216c078ea8c7064487ad1e203a6b
kodebae <heykarmendurbin@gmail.com>
2025-10-08 18:15:57 -0400

    Ch. 10.2: updating users and authorization

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,8 +8,7 @@ pin "controllers", to: "controllers/index.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # jQuery
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
+pin "jquery", to: "https://code.jquery.com/jquery-3.7.1.min.js"
 
-# Bootstrap
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
+# Bootstrap 3
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js"
