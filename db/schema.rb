23635c199caaeeba4c4773cfbebc6135cda9de14
kodebae <heykarmendurbin@gmail.com>
2025-10-14 17:49:20 -0400

    Ch 11: add account activation fields and token generation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_10_13_201534) do
+ActiveRecord::Schema[7.0].define(version: 2025_10_14_213004) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,9 @@ ActiveRecord::Schema[7.0].define(version: 2025_10_13_201534) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
