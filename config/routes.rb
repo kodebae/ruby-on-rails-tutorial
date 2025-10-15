90f0a7e1203e7920e444dd1d3fe85dfb4ae37f3e
kodebae <heykarmendurbin@gmail.com>
2025-10-15 18:57:29 -0400

    ch 11.2 :add account activation via email

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :account_activations, only: [:edit]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
